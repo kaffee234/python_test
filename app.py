72a6a355207b5038ec7da0b94fb74a68422e1a9c
Alex Kalla <Alexander.Kalla@hm.edu>
2024-05-20 22:05:19 +0200

    img up

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
    # Render the page
-   return "Hello Python das ist ein 2 Test der superlativeeeee"
+   return "Hello Python das ist ein 2 Test der"
 
 if __name__ == '__main__':
    # Run the app server on localhost:4449

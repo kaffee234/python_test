d943978f79b1d0fa99432ffa10c0d6791d7ca86d
Alex Kalla <Alexander.Kalla@hm.edu>
2024-05-26 12:06:18 +0200

    Version 1.6.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
    # Render the page
-   return "Hello Python das ist der 30 Test"
+   return "Hello Python das ist der 40 Test des Python Programms !!!!!!!"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000)
